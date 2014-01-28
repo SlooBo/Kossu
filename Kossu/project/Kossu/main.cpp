9cbcf675d0f174177174e2af008a99f262fb16c1
Henri Tiihonen <TTI13SHenriT@kamk.fi>
2014-01-29 01:39:08 +0200

    intial commit for coding

--- Kossu/project/Kossu/main.cpp
+++ Kossu/project/Kossu/main.cpp
@@ -1,7 +1,36 @@
 #include <SFML/Graphics.hpp>
-#include <SFML\Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+
+#include "Mainmenu.h"
+#include "Game.h"
 
 int main()
-{
-	return 0;
+{	
+	Mainmenu menucall;
+	Game gameCall;
+	
+	int state = 1;
+	int *statepoint;
+	statepoint = &state;
+
+	while(1)
+	{
+		std::cout << "Opening Main Menu" << std::endl;
+		menucall.DrawMenu(state);
+		
+		if(state == 2)
+		{
+			std::cout << "perkele" << state << std::endl;
+		}
+
+		if(state == 3)
+		{
+			std::cout << "asd"<< state << std::endl;
+			gameCall.PlayGame();
+		}
+	}
+	system("pause");
+	return EXIT_SUCCESS;
 }
